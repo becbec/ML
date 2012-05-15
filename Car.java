cf6adbbe97c185c20dc87fbbfeb75190f96a5291
Rebecca Wiley <bec.wiley@gmail.com>
2012-05-15 19:14:47 +1000

    Added Car class

---
+++ Car.java
@@ -0,0 +1,11 @@
+package Ass01.ML;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rebecca
+ * Date: 15/05/12
+ * Time: 7:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Car {
+}
